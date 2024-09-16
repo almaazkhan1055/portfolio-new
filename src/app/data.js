67a162318551709be1f11cecef926788ea148c22
Almaaz khan <khan.almaaz1055@gmail.com>
2024-09-16 05:33:12 +0530

    first commit

---
+++ src/app/data.js
@@ -0,0 +1,98 @@
+export const projectsData = [
+  {
+    id: 1,
+    name: "MOVIE-FLIXX",
+    description: "React, Javascript, Redux, Tmdb API, Tailwind css",
+    date: "2023-10-15",
+    demoLink: "https://movie-flixx-chi.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Task-Management",
+    description: "Next, Javascript, Tailwind Css, Context-api",
+    date: "2023-11-20",
+    demoLink: "https://task-management-app-black-beta.vercel.app",
+  },
+  {
+    id: 3,
+    name: "Shopper's-Stop",
+    description: "Next, Javascript, Tailwind Css, Redux",
+    date: "2023-12-10",
+    demoLink: "https://ecommerce-new-theta.vercel.app",
+  },
+  {
+    id: 4,
+    name: "Pingram (Chat-app)",
+    description: " ReactNative, Expo, Firebase, Context Api",
+    date: "2024-01-30",
+    demoLink:
+      "https://expo.dev/preview/update?message=loading%20implemented&updateRuntimeVersion=1.0.0&createdAt=2024-09-13T00%3A28%3A40.398Z&slug=exp&projectId=c5731699-8d6b-422c-9a3c-0e0f750f05c1&group=be21a610-8663-47cc-9d98-75817709174b",
+  },
+  {
+    id: 5,
+    name: "GOOFY-PAWS",
+    description: "React, Javascript, CSS",
+    date: "2024-02-12",
+    demoLink: "https://goofy-paws.vercel.app/",
+  },
+  {
+    id: 6,
+    name: "ORM-DASHBOARD",
+    description: "Nextjs, CSS-in-JS Styled Components",
+    date: "2022-03-1",
+    demoLink: "https://dashboard-xi-sepia.vercel.app",
+  },
+  {
+    id: 7,
+    name: "MAKBYTE.IO",
+    description: "React, Javascript, Redux, Tmdb API, Tailwind css",
+    date: "2023-09-18",
+    demoLink: "https://makbyte.io/",
+  },
+  {
+    id: 8,
+    name: "FACADE-LINE",
+    description: "NEXT.js, Tailwind css",
+    date: "2023-11-05",
+    demoLink: "https://facadeline.co",
+  },
+  {
+    id: 9,
+    name: "TEJAS-ENGINEERING",
+    description: "NEXT.js, CSS3",
+    date: "2023-11-22",
+    demoLink: "https://www.tejasengineeringworks.co.in",
+  },
+  {
+    id: 10,
+    name: "LUXURY-TRD",
+    description: "NEXT.js, CSS3",
+    date: "2023-10-14",
+    demoLink: "https://luxurytrd.com",
+  },
+];
+
+export const BtnList = [
+  { label: "Home", link: "/", icon: "home", newTab: false },
+  { label: "About", link: "/about", icon: "about", newTab: false },
+  { label: "Projects", link: "/projects", icon: "projects", newTab: false },
+  { label: "Contact", link: "/contact", icon: "contact", newTab: false },
+  {
+    label: "Github",
+    link: "https://www.github.com/almaazkhan1055",
+    icon: "github",
+    newTab: true,
+  },
+  {
+    label: "LinkedIn",
+    link: "https://www.linkedin.com/in/almaazkhan",
+    icon: "linkedin",
+    newTab: true,
+  },
+  {
+    label: "Resume",
+    link: "https://almaaz-resume.tiiny.site/",
+    icon: "resume",
+    newTab: true,
+  },
+];
---
+++ src/components/about/index.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import ItemLayout from "./ItemLayout";
+import Link from "next/link";
+import Image from "next/image";
+
+const AboutDetails = () => {
+  return (
+    <section className="py-20 w-full">
+      <div className="grid grid-cols-12 gap-4 xs:gap-6  md:gap-8 w-full">
+        <ItemLayout
+          className={
+            " col-span-full lg:col-span-8 row-span-2 flex-col items-start"
+          }
+        >
+          <h2 className="  text-xl md:text-2xl text-left w-full capitalize">
+            Architect of Enchantment
+          </h2>
+          <p className="font-light  text-xs sm:text-sm md:text-base   ">
+            My journey in web development is powered by an array of mystical
+            tools and languages, with JavaScript casting the core of my
+            enchantments. I wield frameworks like React.js and Next.js with
+            precision, crafting seamless portals (websites) that connect realms
+            (users) across the digital universe. The ancient arts of the
+            Jamstack empower me to create fast, secure, and dynamic experiences,
+            while my design skills ensure every creation is not only functional
+            but visually captivating. Join me as I continue to explore new
+            spells and technologies to shape the future of the web.
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={" col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            Makbyte.io{" "}
+            <sub className="font-semibold text-base">current company</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full xs:col-span-6 lg:col-span-4 text-accent"}
+        >
+          <p className="font-semibold w-full text-left text-2xl sm:text-5xl">
+            1+{" "}
+            <sub className="font-semibold text-base">years of experience</sub>
+          </p>
+        </ItemLayout>
+
+        <ItemLayout
+          className={"col-span-full flex items-center gap-10 justify-center"}
+        >
+          <Image width={72} src={require("../../images/react.webp")} />
+          <Image width={150} src={require("../../images/nextjs.webp")} />
+          <Image width={72} src={require("../../images/redux.webp")} />
+          <Image width={72} src={require("../../images/tailwind.webp")} />
+          <Image width={72} src={require("../../images/css.webp")} />
+          <Image width={72} src={require("../../images/firebase.webp")} />
+          <Image width={72} src={require("../../images/javascript.webp")} />
+          <Image width={72} src={require("../../images/github.webp")} />
+          <Image width={72} src={require("../../images/html.webp")} />
+        </ItemLayout>
+      </div>
+    </section>
+  );
+};
+
+export default AboutDetails;
